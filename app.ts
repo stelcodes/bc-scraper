88026a20e1e37071d067a8958c442bfd6992de0e
Stel Abrego <stel@stel.codes>
2024-03-28 16:23:21 -0700

    Add getText() function for getting text inside elements

--- app.ts
+++ app.ts
@@ -1,7 +1,17 @@
-import puppeteer, { PuppeteerLaunchOptions } from 'puppeteer-core';
+import puppeteer, { ElementHandle, PuppeteerLaunchOptions } from 'puppeteer-core';
 import { Command } from 'commander';
 
 const chromePath = process.env.PUPPETEER_EXECUTABLE_PATH;
+
+async function getText(el: ElementHandle) {
+	return el.evaluate(element => {
+		let text = ''
+		for (var i = 0; i < element.childNodes.length; ++i)
+			if (element.childNodes[i].nodeType === Node.TEXT_NODE)
+				text += element.childNodes[i].textContent;
+		return text.trim()
+	})
+}
 
 async function scrape(urlArg: string, _options: void): Promise<void> {
 	console.log(`Fetching ${urlArg}`);
@@ -14,7 +24,7 @@ async function scrape(urlArg: string, _options: void): Promise<void> {
 	const page = await browser.newPage();
 	await page.goto(url.toString());
 	const trackTitleEl = await page.waitForSelector('h2.trackTitle'); // select the element
-	const trackTitle = await trackTitleEl.evaluate(el => el.textContent); // grab the textContent from the element, by evaluating this function in the browser context
+	const trackTitle = await getText(trackTitleEl); // grab the textContent from the element, by evaluating this function in the browser context
 	console.log(`Track Title: ${trackTitle}`)
 }
 
